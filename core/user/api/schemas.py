54d6fa468f41761f075fa1879d3263ccf4055fe5
Eduardo <dudulj15@gmail.com>
2025-06-30 00:20:53 -0300

    Add user API schemas for data validation and serialization
    - Introduced `UserSchema`, `UserUpdateSchema`, `ContactSchema`, `UserLoginSchema`, and `UserRetrieveSchema` to facilitate data validation and serialization for user-related operations.
    - Enhanced modularity and maintainability of the user API by defining clear schemas for user management, improving overall code structure and usability.

---
+++ core/user/api/schemas.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import List
+
+from ninja import ModelSchema, Schema
+
+from user.models import Contact, User
+
+
+class UserSchema(ModelSchema):
+    contacts: List["ContactSchema"]
+
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
+
+
+class UserUpdateSchema(ModelSchema):
+    class Meta:
+        model = User
+        fields = ("first_name", "email")
+
+
+class ContactSchema(ModelSchema):
+    class Meta:
+        model = Contact
+        fields = ("id", "value", "type")
+
+
+class UserLoginSchema(Schema):
+    username: str
+    password: str
+
+
+class UserRetrieveSchema(Schema):
+    id: int
+    username: str
+    cpf: str
+    name: str
+    email: str
+    last_login: datetime | None
+    created_at: datetime
+    updated_at: datetime
+    contacts: List[ContactSchema]
