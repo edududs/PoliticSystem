9157902569624ee8d2936e65410533b3b58a945e
Eduardo <dudulj15@gmail.com>
2025-06-29 23:50:46 -0300

    Refactor user mixins to enhance functionality and type safety
    - Introduced `UserNameMixin` and `UserEmailMixin` to separate concerns related to user name and email functionalities.
    - Updated `get_display_name` method to improve handling of user name retrieval, ensuring it gracefully falls back to first and last names.
    - Created a new `UserMixin` class that aggregates both mixins, promoting code reusability and clarity.
    - Applied generics to mixins for better type safety and flexibility in usage.

--- core/user/utils/models_mixins.py
+++ core/user/utils/models_mixins.py
@@ -1,14 +1,28 @@
-from django.db import models
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
 
 
-class UserMixin(models.Model):
-    """Mixin para adicionar display name ao usuário."""
+class UserNameMixin(Generic[T]):
+    """Mixin para funcionalidades relacionadas ao nome do usuário."""
 
-    def get_display_name(self):
+    def get_display_name(self: T) -> str:
         """Retorna o nome completo ou o primeiro nome do usuário."""
-        return self.get_full_name() or self.first_name  # type: ignore
+        if full_name := getattr(self, "get_full_name", lambda: None)():
+            return full_name
+        first_name = getattr(self, "first_name", "")
+        last_name = getattr(self, "last_name", "")
+        return f"{first_name} {last_name}".strip()
 
-    class Meta:
-        """Meta options for UserMixin."""
 
-        abstract = True
+class UserEmailMixin(Generic[T]):
+    """Mixin para funcionalidades relacionadas ao email do usuário."""
+
+    def get_email_domain(self: T) -> str:
+        """Retorna o domínio do email do usuário."""
+        email = getattr(self, "email", "")
+        return email.split("@")[1] if "@" in email else ""
+
+
+class UserMixin(UserNameMixin[T], UserEmailMixin[T]):
+    """Agregador de todos os mixins relacionados ao usuário."""
