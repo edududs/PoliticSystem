ca140157d3abbe13aecefe36d45d668eaa21d2dd
Eduardo <dudulj15@gmail.com>
2025-06-29 23:57:59 -0300

    Add API route for new endpoint
    - Included a new route for `api2/` in `urls.py`, integrating the `api` module's URLs for enhanced API functionality.

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .api import api
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("select2/", include("django_select2.urls")),
     path("api/", include("user.urls")),
+    path("api2/", api.urls),
 ]
